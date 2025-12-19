671063b70be1424717f0f790334b4ab5e9dd3dd4
Wojciech Knott <wojciechknott@gmail.com>
2025-12-19 17:28:20 +0100

    feat: add manifest file and update layout to include scoring header and app icons

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Navigation from "@/ui/molecues/Navigation";
+import { ScoringHeader } from "@/ui/molecues/ScoringHeader";
 
 const geistSans = Geist({
 	variable: "--font-geist-sans",
@@ -14,8 +14,19 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-	title: "Wyniczek",
-	description: "Wyniczek - aplikacja do zarządzania wynikami gier planszowych",
+	title: "Wynniczek",
+	description: "Wynniczek - aplikacja do zarządzania wynikami gier planszowych",
+	icons: {
+		icon: [
+			{ url: "/favicon-16x16.png", sizes: "16x16", type: "image/jpeg" },
+			{ url: "/favicon-32x32.png", sizes: "32x32", type: "image/jpeg" },
+		],
+		apple: [{ url: "/apple-touch-icon.png", sizes: "180x180", type: "image/jpeg" }],
+		other: [
+			{ rel: "icon", url: "/android-icon-192x192.png", sizes: "192x192", type: "image/jpeg" },
+		],
+	},
+	manifest: "/manifest.json",
 };
 
 export default function RootLayout({
@@ -25,7 +36,10 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en" className="dark" data-theme="dark">
-			<body className={`${geistSans.variable} ${geistMono.variable} antialiased bg-background text-foreground`}>
+			<body
+				className={`${geistSans.variable} ${geistMono.variable} bg-background text-foreground antialiased`}
+			>
+				<ScoringHeader />
 				{children}
 			</body>
 		</html>
