953085537b1f6d0e42d397259cbf6dd3ec9ba8ed
Wojciech Knott <wojciechknott@gmail.com>
2025-12-20 10:55:49 +0100

    feat: graphql config fix

--- graphql.config.ts
+++
@@ -1,8 +0,0 @@
-import type { IGraphQLConfig } from "graphql-config";
-
-const config: IGraphQLConfig = {
-    schema: "http://localhost:3000/graphql",
-    documents: "graphql/*.graphql",
-};
-
-export default config;
