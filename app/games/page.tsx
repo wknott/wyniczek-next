72a8817b11e397d9ec286647cfaac2e52e9bceff
Wojciech Knott <wojciechknott@gmail.com>
2026-04-12 06:51:36 +0200

    feat: Refactor GamesPage layout for improved responsiveness and readability

--- app/games/page.tsx
+++ app/games/page.tsx
@@ -6,28 +6,27 @@ import { GameSortBy } from "@/gql/graphql";
 import { Suspense } from "react";
 
 interface PageProps {
-    searchParams: Promise<{ sortBy?: GameSortBy }>;
+	searchParams: Promise<{ sortBy?: GameSortBy }>;
 }
 
 export default async function GamesPage({ searchParams }: PageProps) {
-    await connection();
+	await connection();
 
-    const { sortBy = "POPULARITY" } = await searchParams;
-    const initialData = await getGamesPage(sortBy);
-    const fetchPage = getGamesPage.bind(null, sortBy);
+	const { sortBy = "POPULARITY" } = await searchParams;
+	const initialData = await getGamesPage(sortBy);
+	const fetchPage = getGamesPage.bind(null, sortBy);
 
-    return (
-        <main className="mx-auto flex min-h-screen max-w-6xl flex-col items-center gap-6 p-2 sm:p-4 md:p-6">
-            <div className="flex w-full items-center justify-between">
-                <h1 className="text-4xl font-bold">Moje Gry</h1>
-                <Suspense fallback={null}>
-                    <SortSelect defaultValue={sortBy} />
-                </Suspense>
-            </div>
-            <div className="w-full">
-                <GamesList initialData={initialData} onPageChange={fetchPage} />
-            </div>
-        </main>
-    );
+	return (
+		<main className="mx-auto flex min-h-screen max-w-6xl flex-col items-center gap-6 p-2 sm:p-4 md:p-6">
+			<div className="flex w-full flex-col gap-2 md:flex-row md:items-center md:justify-between">
+				<h1 className="text-4xl font-bold">Moje Gry</h1>
+				<Suspense fallback={null}>
+					<SortSelect defaultValue={sortBy} />
+				</Suspense>
+			</div>
+			<div className="w-full">
+				<GamesList initialData={initialData} onPageChange={fetchPage} />
+			</div>
+		</main>
+	);
 }
-
