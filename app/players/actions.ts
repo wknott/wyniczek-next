564b9c4c4cbfc3518ce6d13536e03c9196473d96
Wojciech Knott <wojciechknott@gmail.com>
2026-04-20 14:52:03 +0200

    style: Improve code formatting and consistency in actions.ts

--- app/players/actions.ts
+++ app/players/actions.ts
@@ -3,36 +3,32 @@
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 import { executeGraphql } from "@/api/executeGraphql";
-import {
-    CreatePlayerDocument,
-    GetPlayersDocument,
-    GetPlayerByIdDocument,
-} from "@/gql/graphql";
+import { CreatePlayerDocument, GetPlayersDocument, GetPlayerByIdDocument } from "@/gql/graphql";
 
 export async function createPlayer(formData: FormData) {
-    const name = formData.get("name") as string;
-    const userId = "1234";
+	const name = formData.get("name") as string;
+	const userId = "1234";
 
-    if (!name) return;
+	if (!name) return;
 
-    await executeGraphql(CreatePlayerDocument, {
-        input: {
-            name,
-            userId,
-        },
-    });
+	await executeGraphql(CreatePlayerDocument, {
+		input: {
+			name,
+			userId,
+		},
+	});
 
-    revalidatePath("/");
-    revalidatePath("/players");
-    redirect("/players");
+	revalidatePath("/");
+	revalidatePath("/players");
+	redirect("/players");
 }
 
 export async function getPlayers() {
-    const { players } = await executeGraphql(GetPlayersDocument, {});
-    return players;
+	const { players } = await executeGraphql(GetPlayersDocument);
+	return players;
 }
 
 export async function getPlayerById(id: string) {
-    const { player } = await executeGraphql(GetPlayerByIdDocument, { id });
-    return player;
+	const { player } = await executeGraphql(GetPlayerByIdDocument, { id });
+	return player;
 }
